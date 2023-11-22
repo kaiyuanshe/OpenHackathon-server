96dfd8b16e843c433722102bd9d09dea91d39b9c
South Drifted <shiy2008@gmail.com>
2023-11-22 01:52:22 +0000

    [add] Azure Blob configuration & GitHub Actions badge [optimize] update Meta Data

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,10 +1,27 @@
-export default () => ({
-  transformer: {
-    enabled: true,
-    responseTransforms: {
-      removeDataKey: true,
-      removeAttributesKey: true,
+export default ({ env }) => {
+  const serviceBaseURL = env('AZURE_BLOB_BASE_URL');
+  const [_, account] = serviceBaseURL.split(/\W+/);
+
+  return {
+    transformer: {
+      enabled: true,
+      responseTransforms: {
+        removeDataKey: true,
+        removeAttributesKey: true,
+      },
     },
-  },
-  'react-icons': true,
-});
+    'react-icons': true,
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-azure-storage',
+        providerOptions: {
+          serviceBaseURL,
+          account,
+          accountKey: env('AZURE_BLOB_ACCOUNT_KEY'),
+          containerName: 'file',
+          defaultPath: 'assets',
+        },
+      },
+    },
+  };
+};
